5284b71ae19f9d14bf91206c8f161d713e67b0bd
nam203 <cuciuiu357@gamil.com>
2023-09-21 21:56:41 +0700

    setup server

---
+++ server/src/services/UserService.js
@@ -0,0 +1,10 @@
+const createUser =()=>{
+    return new Promise ((resolve, reject)=>{
+        try{
+resolve({})
+        } catch(e){
+            reject(e)
+        }
+    })
+}
+module.exports= {createUser}
