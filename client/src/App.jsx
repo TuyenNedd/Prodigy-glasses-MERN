36940a68f7e1a54cbcaa543b9cb5fa20cfd693fb
TuyenNedd <trituyen2003@gmail.com>
2023-09-22 11:44:41 +0700

    setup SCSS, fontface, fontfamily, color variables

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
-import "./App.css";
-import MyComponent from "./components/api.jsx";
+import "./App.scss";
+import Testing from "./components/Testing.jsx";
+// import MyComponent from "./components/api.jsx";
 
 function App() {
-  return <>
-    <MyComponent></MyComponent>
-  </>;
+  return (
+    <>
+      {/* <MyComponent></MyComponent> */}
+      <Testing></Testing>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/Testing.jsx
@@ -0,0 +1,10 @@
+
+const Testing = () => {
+    return (
+        <div>
+            <h1 className="ITCGara text-8xl">Testing</h1>
+        </div>
+    );
+};
+
+export default Testing;
