d7917e4d917d3dd90d344a06d29b9ea5e71c13ad
nam203 <cuciuiu357@gamil.com>
2023-09-28 23:44:59 +0700

    uptdateacc

--- server/src/routes/ProductRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const  router = express.Router()
-const ProductController = require('../controllers/ProductController')
-const {authMiddleWare} = require('../middleware/authMiddleWare')
-router.post("/create",ProductController.createProduct )
-router.put("/update/:id",authMiddleWare,ProductController.updateProduct)
-
-module.exports = router
