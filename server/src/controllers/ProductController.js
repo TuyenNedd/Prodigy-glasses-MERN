689390e1928e48e838bb184a1c8cfed75f43b2ed
nam203 <cuciuiu357@gamil.com>
2023-09-29 22:01:52 +0700

    upgetdetails

--- server/src/controllers/ProductController.js
+++ server/src/controllers/ProductController.js
@@ -50,7 +50,31 @@ const updateProduct = async (req, res) => {
     });
   }
 };
+const getDetailsProduct = async (req, res) => {
+  try {
+      const productId = req.params.id
+
+    
+      if (!productId) {
+          return res.status(200).json({
+              status: 'ERR',
+              message: 'The userId is required'
+          })
+
+      }
+      
+      const response = await ProductService.getDetailsProduct(productId)
+     
+      return res.status(200).json(response)
+     
+  } catch (e) {
+      return res.status(404).json({
+          message: e
+      })
+  }
+}
 module.exports = {
   createProduct,
   updateProduct,
+  getDetailsProduct
 };
--- server/src/routes/ProductRouter.js
+++ server/src/routes/ProductRouter.js
@@ -4,5 +4,7 @@ const ProductController = require('../controllers/ProductController')
 const {authMiddleWare} = require('../middleware/authMiddleWare')
 router.post("/create",ProductController.createProduct )
 router.put("/update/:id",authMiddleWare,ProductController.updateProduct)
-
+router.get("/get-details/:id",ProductController.getDetailsProduct)
+router.delete('/delete-product/:id', ProductController.deleteProduct)
+router.get("/get-all/",ProductController.getAllProducts)
 module.exports = router
