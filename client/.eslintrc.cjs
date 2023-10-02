30cf138b77c84eb1c6e03332c9d0e760b58caef4
TuyenNedd <trituyen2003@gmail.com>
2023-10-02 15:22:29 +0700

    set up eslint, flowbite

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,20 +1,78 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "react"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    // indent: ["error", "space"],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
+
+// module.exports = {
+//   root: true,
+//   env: {
+//     browser: true,
+//     es2021: true,
+//   },
+//   extends: [
+//     "eslint:recommended",
+//     "plugin:react/recommended",
+//     "plugin:react/jsx-runtime",
+//     "plugin:react-hooks/recommended",
+//   ],
+//   ignorePatterns: ["dist", ".eslintrc.cjs"],
+//   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+//   settings: { react: { version: "18.2" } },
+//   overrides: [
+//     {
+//       env: {
+//         node: true,
+//       },
+//       files: [".eslintrc.{js,cjs}"],
+//       parserOptions: {
+//         sourceType: "script",
+//       },
+//     },
+//   ],
+//   parserOptions: {
+//     ecmaVersion: "latest",
+//     sourceType: "module",
+//   },
+//   plugins: ["react, react-refresh"],
+//   rules: {
+//     indent: ["error", "space"],
+//     "linebreak-style": ["error", "windows"],
+//     quotes: ["error", "double"],
+//     semi: ["error", "always"],
+//     "react-refresh/only-export-components": [
+//       "warn",
+//       { allowConstantExport: true },
+//     ],
+//   },
+// };
