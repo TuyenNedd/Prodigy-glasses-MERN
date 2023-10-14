be48948b162736345cd166b70aa02623af479807
nam203 <cuciuiu357@gamil.com>
2023-10-14 17:50:59 +0700

    update logout

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -185,19 +185,19 @@ const refreshToken = async (req, res) => {
 }
 
 
-// const logoutUser = async (req, res) => {
-//     try {
-//         res.clearCookie('refresh_token')
-//         return res.status(200).json({
-//             status: 'OK',
-//             message: 'Logout successfully'
-//         })
-//     } catch (e) {
-//         return res.status(404).json({
-//             message: e
-//         })
-//     }
-
+const logoutUser = async (req, res) => {
+    try {
+        res.clearCookie('refresh_token')
+        return res.status(200).json({
+            status: 'OK',
+            message: 'Logout successfully'
+        })
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
 module.exports = {
     createUser,
     loginUser,updateUser,
@@ -205,6 +205,6 @@ module.exports = {
     getAllUser,
     getDetailsUser,
     refreshToken,
-    // logoutUser,
+    logoutUser,
     // deleteMany
 }
