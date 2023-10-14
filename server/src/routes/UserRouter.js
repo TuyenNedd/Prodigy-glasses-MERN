e9130e925251852343936c7361e77f5126f8abe8
nam203 <cuciuiu357@gamil.com>
2023-10-14 17:50:59 +0700

    update logout

--- server/src/routes/UserRouter.js
+++ server/src/routes/UserRouter.js
@@ -5,7 +5,7 @@ const { authMiddleWare, authUserMiddleWare } = require("../middleware/authMiddle
 
 router.post('/sign-up', userController.createUser)
 router.post('/sign-in', userController.loginUser)
-// router.post('/log-out', userController.logoutUser)
+router.post('/log-out', userController.logoutUser)
 router.put('/update-user/:id', userController.updateUser)
 router.delete('/delete-user/:id',authMiddleWare, userController.deleteUser)
 router.get('/getAll', authMiddleWare, userController.getAllUser)
