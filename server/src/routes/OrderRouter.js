1bfa5c3d655df450e81a2c584ebec4a0ed059eea
nam203 <cuciuiu357@gamil.com>
2023-10-16 15:58:02 +0700

    up cancel oder

--- server/src/routes/OrderRouter.js
+++ server/src/routes/OrderRouter.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const  router = express.Router()
 const OrderController = require('../controllers/OrderController')
-const {authUserMiddleWare} = require('../middleware/authMiddleWare')
+const {authUserMiddleWare,authMiddleWare} = require('../middleware/authMiddleWare')
 router.post("/create",authUserMiddleWare,OrderController.createOrder )
-// router.put("/update/:id",authMiddleWare,ProductController.updateProduct)
 router.get("/get-all-order/:id",authUserMiddleWare,OrderController.getAllDetailsOrder)
-router.get("/get-details-order/:id",OrderController.getDetailsOrder)
+router.get("/get-details-order/:id",authUserMiddleWare,OrderController.getDetailsOrder)
 router.delete('/cancel-order/:id',authUserMiddleWare, OrderController.cancelOrderDetails)
 
-// router.get("/get-all/",ProductController.getAllProducts)
+router.get('/get-all-order',authMiddleWare, OrderController.getAllOrder)
+
 module.exports = router
