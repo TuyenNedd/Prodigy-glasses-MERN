24121b18c3e75e37b812eaa1672d47ea4dbcb10d
nam203 <cuciuiu357@gamil.com>
2023-10-16 17:59:30 +0700

    update get all admin

--- server/src/controllers/OrderController.js
+++ server/src/controllers/OrderController.js
@@ -73,10 +73,22 @@ const createOrder = async (req, res) => {
         })
     }
 }
+const getAllOrder = async (req, res) => {
+    try {
+        const data = await OrderService.getAllOrder()
+        return res.status(200).json(data)
+    } catch (e) {
+        // console.log(e)
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
 
 module.exports = {
   createOrder,
   getAllProducts,
   getDetailsOrder,
-  cancelOrderDetails
+  cancelOrderDetails,
+  getAllOrder
 };
--- server/src/services/OrderService.js
+++ server/src/services/OrderService.js
@@ -169,9 +169,24 @@ const getAllOrderDetails = (id) => {
         }
     })
 }
+const getAllOrder = () => {
+  return new Promise(async (resolve, reject) => {
+      try {
+          const allOrder = await Order.find().sort({createdAt: -1, updatedAt: -1})
+          resolve({
+              status: 'OK',
+              message: 'Success',
+              data: allOrder
+          })
+      } catch (e) {
+          reject(e)
+      }
+  })
+}
 module.exports={
     createOrder,
     getAllOrderDetails,
     getOrderDetails,
-    cancelOrderDetails
+    cancelOrderDetails,
+    getAllOrder
 }
