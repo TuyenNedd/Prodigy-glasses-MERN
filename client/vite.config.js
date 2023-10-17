88fb5b146facc336ea33f7cc8977d6652c0cb092
vietnqdev-543 <vietnq543@gmail.com>
2023-10-18 01:50:35 +0700

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dns from "dns";
-
-dns.setDefaultResultOrder("verbatim");
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3001,
-    // proxy: {
-    //   "/api": {
-    //     target: "http://localhost:3001", // Đổi cổng nếu cần
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, ""),
-    //   },
-    // },
-  },
-  // server: {
-  // },
-});
+})
