97b312d31d632b250d6b6955be3033d1cecf164a
conghuy2003 <dangconghuypro@gmail.com>
2023-11-09 12:12:36 +0700

    upload PaymentPage

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,7 +10,24 @@ export default withMT({
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xl": { max: "1535px" },
+        // => @media (max-width: 1535px) { ... }
+
+        xl: { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+
+        lg: { max: "960px" },
+        // => @media (max-width: 1023px) { ... }
+
+        md: { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+
+        sm: { max: "639px" },
+        // => @media (max-width: 639px) { ... }
+      },
+    },
   },
   plugins: [flowbitePlugin],
 });
